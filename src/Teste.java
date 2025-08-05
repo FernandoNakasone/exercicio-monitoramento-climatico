9927f0cf734e219899b232b61353b79568c82e54
FernandoNakasone <fernandonakasone040@gmail.com>
2025-08-05 10:01:26 -0300

    atualização do exercicio, nova classe Sensor

--- src/Teste.java
+++ src/Teste.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Teste {
     public static void main(String[] args) {
 
@@ -7,8 +10,21 @@ public class Teste {
             validar(temp);
         }
         catch(LeituraInvalidaException e){
-            System.out.println(e);
+            System.out.println(e.getMessage());
         }
+        finally {
+            System.out.println("X");
+        }
+
+        Sensor sensor = new Sensor("1");
+        List<Double> lista = new ArrayList<>();
+
+        for(int i=0; i<100; i++){
+           lista.add((double) Math.round(Math.random() * 61));
+        }
+
+        sensor.registrarLeituras(lista);
+
     }
 
     private static void validar(double temp){
