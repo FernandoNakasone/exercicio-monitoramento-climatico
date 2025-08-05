71c8b4bd0b69470b47ebe01a02606d8796bc8717
FernandoNakasone <fernandonakasone040@gmail.com>
2025-08-05 09:19:27 -0300

    Exercicio monitoramento climatico

--- src/Teste.java
+++ src/Teste.java
@@ -2,7 +2,13 @@ public class Teste {
     public static void main(String[] args) {
 
         double temp = 100;
-        validar(temp);
+
+        try {
+            validar(temp);
+        }
+        catch(LeituraInvalidaException e){
+            System.out.println(e);
+        }
     }
 
     private static void validar(double temp){
