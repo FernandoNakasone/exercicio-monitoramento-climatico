2f4cbae4c12a36d51fa9621143767896fb92884e
FernandoNakasone <fernandonakasone040@gmail.com>
2025-08-05 09:14:51 -0300

    Teste

--- src/Teste.java
+++ src/Teste.java
@@ -1,2 +1,13 @@
 public class Teste {
+    public static void main(String[] args) {
+
+        double temp = 100;
+        validar(temp);
+    }
+
+    private static void validar(double temp){
+        if(temp < -50 || temp > 50){
+            throw new LeituraInvalidaException(12, temp);
+        }
+    }
 }
