9d7579f61ca67c807e22e58eba4d9f5e4f488c3c
FernandoNakasone <fernandonakasone040@gmail.com>
2025-08-05 10:40:41 -0300

    classes ErroSensor e SistemaMonitoramento

---
+++ src/SistemaMonitoramento.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class SistemaMonitoramento {
+    private List<Sensor> sensoresValidos;
+    private List<ErroSensor> sensoresComErro;
+
+    public void exibirRelatorio(){
+
+    }
+
+    public void processarSensores(List<Sensor> listSensor, List<List<Double>> lista){
+
+        for(Sensor s : listSensor){
+
+        }
+    }
+}
