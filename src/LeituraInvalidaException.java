0a9d1dca9cb63edab22218b52275180747ad33c8
FernandoNakasone <fernandonakasone040@gmail.com>
2025-08-05 09:04:21 -0300

    exercicio monitoramento

--- src/LeituraInvalidaException.java
+++ src/LeituraInvalidaException.java
@@ -1,6 +1,6 @@
 public class LeituraInvalidaException extends RuntimeException {
     public LeituraInvalidaException(int index, double temperatura) {
 
-      super("Leitura inválida na posição");
+      super("Leitura inválida na posição " + index + ": " + temperatura);
     }
 }
